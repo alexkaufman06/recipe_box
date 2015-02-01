3b5a9f4a68b18447b1bf522ee106a5de08fb5648
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-01 09:52:40 -0800

    Update files with syntactic sugar

--- spec/ingredients_spec.rb
+++ spec/ingredients_spec.rb
@@ -1,4 +1,4 @@
-require("spec_helper")
+require "spec_helper"
 
 describe(Ingredient) do
   it { should have_and_belong_to_many(:recipes) }
--- spec/recipes_spec.rb
+++ spec/recipes_spec.rb
@@ -1,17 +1,17 @@
-require("spec_helper")
+require "spec_helper"
 
-describe(Recipe) do
+describe Recipe do
   it { should have_and_belong_to_many(:ingredients) }
   it { should have_and_belong_to_many(:categories) }
   # The above two lines are specs for 'associations'
 
-  it("capitalizes the recipe name before initializaion") do
+  it "capitalizes the recipe name before initializaion" do
     test_recipe = Recipe.create(:name => "quiche")
     expect(test_recipe.name).to(eq("Quiche"))
     # Above is a spec for a 'callback'
   end
 
-  it("validates the presence of a name") do
+  it "validates the presence of a name" do
     test_recipe = Recipe.new(:name => "")
     expect(test_recipe.save).to(eq(false))
     # Above is a spec for a 'validation'
